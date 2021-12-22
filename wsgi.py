a253da645957510fea156f55f03be6376b01384f
HarryJohn <harijaona@digital.gov.mg>
2021-12-22 13:04:46 +0300

    Create product

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-docstring
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from config import Config
 
 from flask_migrate import Migrate
@@ -25,7 +25,7 @@ from schemas import many_product_schema, one_product_schema
 
 @app.route(f'{Config.BASE_URL}/products', methods=['GET'])
 @app.route(f'{Config.BASE_URL}/products/<int:id>', methods=['GET'])
-def get_many_product(id=None):
+def get_products(id=None):
     if id:
         product = db.session.query(Product).get(id) # SQLAlchemy request => 'SELECT * FROM products WHERE id = {id}'
         if product: 
@@ -35,3 +35,20 @@ def get_many_product(id=None):
     else:
         products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
         return many_product_schema.jsonify(products), 200
+
+@app.route(f'{Config.BASE_URL}/products', methods=['POST'])
+def create_product():
+
+    data = request.json
+
+    if isinstance(data, dict) and data.get("name"):
+        product = Product()
+        product.name = data.get("name")
+        product.description = data.get("description")
+
+        db.session.add(product)
+        db.session.commit()
+
+        return one_product_schema.jsonify(product), 200
+    else:
+        return jsonify({"message": "Unprocessable Entity" }), 422
